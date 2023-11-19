b199778395f7e17b709c8ecc5fba4172789d7998
Arseniy Olevskiy <arsidian@gmail.com>
2023-11-19 13:28:39 +0400

    calculator changes commit

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Mixed Fraction Calculator by Calculator.iO
+Plugin Name: Mixed Fraction Calculator by www.calculator.io
 Plugin URI: https://www.calculator.io/mixed-fraction-calculator/
 Description: Mixed number to improper fraction calculator to convert mixed numbers, consisting of a whole number and a proper fraction, to improper fractions.
 Version: 1.0.0
@@ -16,7 +16,7 @@ if (!function_exists('add_shortcode')) return "No direct call for Mixed Fraction
 
 function display_ci_mixed_fraction_calculator(){
     $page = 'index.html';
-    return '<h2><a href="https://www.calculator.io/mixed-fraction-calculator/" target="_blank"><img src="' . esc_url(plugins_url('assets/images/icon-48.png', __FILE__ )) . '" width="48" height="48"></a> Mixed Fraction Calculator</h2><div><iframe style="background:transparent; overflow: scroll" src="' . esc_url(plugins_url($page, __FILE__ )) . '" width="100%" frameBorder="0" allowtransparency="true" onload="this.style.height = this.contentWindow.document.documentElement.scrollHeight + \'px\';" id="ci_mixed_fraction_calculator_iframe"></iframe></div>';
+    return '<h2><img src="' . esc_url(plugins_url('assets/images/icon-48.png', __FILE__ )) . '" width="48" height="48">Mixed Fraction Calculator</h2><div><iframe style="background:transparent; overflow: scroll" src="' . esc_url(plugins_url($page, __FILE__ )) . '" width="100%" frameBorder="0" allowtransparency="true" onload="this.style.height = this.contentWindow.document.documentElement.scrollHeight + \'px\';" id="ci_mixed_fraction_calculator_iframe"></iframe></div>';
 }
 
 add_shortcode( 'ci_mixed_fraction_calculator', 'display_ci_mixed_fraction_calculator' );
--- readme.md
+++ readme.md
@@ -7,7 +7,7 @@ Mixed number to improper fraction calculator to convert mixed numbers, consistin
 
 ## Installation
 
-1. [Download](https://github.com/pub-calculator-io/age-calculator/archive/refs/heads/master.zip) the ZIP file of this repository.
+1. [Download](https://github.com/pub-calculator-io/mixed-fraction-calculator/archive/refs/heads/master.zip) the ZIP file of this repository.
 2. Upload the /mixed-fraction-calculator-master/ folder to the /wp-content/plugins/ directory.
 3. Activate the [Mixed Fraction Calculator](https://www.calculator.io/mixed-fraction-calculator/ "Mixed Fraction Calculator Homepage") plugin through the "Plugins" menu in WordPress.
 
